88865ac7afd6626c7b0363b9b03c91f2c9f47e22
paul <paul.henrich@gmail.com>
2011-02-01 22:16:32 -0500

    split out javascript into separate file

---
+++ crafts.js
@@ -0,0 +1,77 @@
+var crafts = (function(){
+
+    var first  = [], second = [], sizes  = [], colors = [];
+
+    first =
+       ['arugula',
+        'ibexes',
+        'menorahs',
+        'traffic cones',
+        'absinthe spoons',
+        'cardboard boxes',
+        'geoducks',
+        'nickelodeons',
+        'unibrows',
+        'rollerblades',
+        'poison darts',
+        'tapirs',
+        'corneas',
+        'mothballs',
+        'cummerbunds',
+        'chicken feet',
+        'crawdads',
+        'oubliettes',
+        'freshwater lakes',
+        'pie servers',
+        'brogues',
+        'cigar cutters',
+        'ballots',
+        ];
+
+    second =
+       ['narwhals',
+        'owls',
+        'sparrows',
+        'sushi rolls',
+        'vintage glasses',
+        'brass knuckles',
+        'steampunks',
+        'squids',
+        'mustaches',
+        'beards',
+        'bicycles',
+        'silhouettes',
+        'typewriter keys',
+        'tree branches',
+        'chandeliers ',
+        'macrons',
+        'cupcakes',
+        'kittens',
+        'wreaths',
+        'pennants',
+        'eggs',
+        'camper vans',
+        'anatomically correct hearts',
+        ];
+
+    //sizes = [122];
+    colors= ["#383838", "#66B1C0", "#FF368D", "#B9D15A"];
+
+    function _rand_index(target_array_ref){
+        return Math.floor(target_array_ref.length * Math.random());
+    }
+
+    function narwahls_are_so_passe(){
+        var crafts_div, sentence;
+        sentence   = first[_rand_index(first)] + " are the new " + second[_rand_index(second)];
+        crafts_div = document.getElementById("crafts");
+
+        //crafts_div.style.fontSize = sizes[_rand_index(sizes)] + "px";
+        crafts_div.style.color    = colors[_rand_index(colors)];
+        crafts_div.innerHTML      = sentence;
+    }
+
+    return narwahls_are_so_passe;
+})();
+
+window.onload = crafts;
